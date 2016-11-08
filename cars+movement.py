7a411a0c8aaa2c80c1edc1ba6f81885f04001a87
61pancakes <61pancakes@users.noreply.github.com>
2016-11-08 16:04:19 +0100

    Uploading code for car + start of move function

---
+++ cars+movement.py
@@ -0,0 +1,75 @@
+class CarHorizontal(object):
+    def __init__(self, board):
+        self.board = board
+        self.position = position
+
+    def getCarPosition(self):
+        return self.pos
+
+    def setCarPosition(self, position):
+        self.pos = position
+
+    def updatePosition(self, position):
+        // Ik weet niet of dit een functie moet zijn of class? //
+
+class CarRed(object):
+    def __init__(self, board):
+        self.board = board
+        self.position = position
+
+    def getCarPosition(self):
+        return self.pos
+
+    def setCarPosition(self, position):
+        self.pos = position
+
+    def updatePosition(self, position):
+        // Ik weet niet of dit een functie moet zijn of class? //
+
+def moveNorth(car, steps):
+
+# get position car
+
+# check if new position is in room
+
+# check if column is empty for n steps
+
+# move car n steps
+
+# give error if movement is impossible
+
+def moveEast(car, steps):
+
+# get position car
+
+# check if new position is in room
+
+# check if row is empty for n steps
+
+# move car n steps
+
+# give error if movement is impossible
+
+def moveSouth(car, steps):
+
+# get position car
+
+# check if new position is in room
+
+# check if column is empty for n steps
+
+# move car n steps
+
+# give error if movement is impossible
+
+def moveWest(car, steps):
+
+# get position car
+
+# check if new position is in room
+
+# check if row is empty for n steps
+
+# move car n steps
+
+# give error if movement is impossible
