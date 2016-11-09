34a3edbc6c6e83600dcad22f0138a9f8512376a2
61pancakes <61pancakes@users.noreply.github.com>
2016-11-09 13:58:47 +0100

    Move North gemaakt

--- cars+movement.py
+++ cars+movement.py
@@ -10,9 +10,6 @@ class CarHorizontal(object):
     def setCarPosition(self, position):
         self.pos = position
 
-    def updatePosition(self, position):
-        // Ik weet niet of dit een functie moet zijn of class? //
-
 class CarRed(object):
     def __init__(self, board, size):
         self.board = board
@@ -25,21 +22,36 @@ class CarRed(object):
     def setCarPosition(self, position):
         self.pos = position
 
-    def updatePosition(self, position):
-        // Ik weet niet of dit een functie moet zijn of class? //
+def moveNorth(carID, steps):
+    
+    # check if car orientation is Vertical
+    if self.Vehicle.length == 2:
+        length = 2
+    else:
+        length = 3
+        
+    # get car position
+    old_x = board.position.getX()
+    old_y = board.position.getY()
 
-def moveNorth(car, steps):
-
-# get position car
-
-# check if new position is in room
-
-# check if column is empty for n steps
-
-# move car n steps
-
-# give error if movement is impossible
-
+    # check if new position is in room
+    new_location = (old_x, (old_y + steps))
+    if self.board.isPositionOnBoard(new_location) != True
+        return False
+    
+    # check if column is empty for n steps
+    else:
+        for each i in range(0, steps):
+            if (y + i) != '-':
+                return False
+            
+    # move car steps op een manier?        
+    for each i in range (0, steps):
+        swap (x, y) met (x, y + 1)
+        swap (x, y - 1) met (x, y)
+        if length = 3:
+            swap (x, y - 2) met (x, y - 1)
+    
 def moveEast(car, steps):
 
 # get position car
