c17b1b65f09edfc4e75676c7ee99357fe81a6789
lisahabermehl <habermehl.lisa@gmail.com>
2016-11-15 09:02:50 +0100

    Update vehicle.py

--- vehicle.py
+++ vehicle.py
@@ -1,27 +1,26 @@
 class Vehicle(object):
     def __init__(self, id, x, y, orientation):
         self.id = id
+        
+        # we'll use length = 2 for now
         self.length = 2
-
+        
         self.x = x
         self.y = y
-
-        if orientation == 'H':
-            self.orientation = orientation
-            x_end = self.x + (self.length - 1)
-            y_end = self.y
-        elif orientation == 'V':
-            self.orientation = orientation
-            x_end = self.x
-            y_end = self.y + (self.length - 1)
+        self.orientation = orientation
 
     def vehicleInfo(self):
         return "Vehicle({0}, {1}, {2}, {3})".format(self.id, self.x, self.y, self.orientation)
 
-# class VehicleHorizontal(Vehicle):
-#     def __init__(self, id, x, y):
-#         self.id = id
-#         self.x = x
-#         self.y = y
-#     def getCarPosition(self):
-#         return self.pos
+# een class 'move' en locatie op board
+class Move(Vehicle):
+    def __init__(id, x, y, orientation):
+        self.id = id
+        self.x = x
+        self.y = y
+        self.orientation = orientation
+    # is de vehicle movable?
+        # bij elke move wat toevoegen aan queue
+        # BreadthFirst limiten tot hoe diep die gaat
+    # gaan we het vanuit de muur of vanuit de auto doen?
+    # houd rekening met dat 1 stap 4/3/2/1 blokken lang kan zijn
