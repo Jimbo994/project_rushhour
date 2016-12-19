2a2dfdcb1692f9b676274937fcf382b73039ee8a
Jimbo994 <jim.boelrijk@gmail.com>
2016-12-19 13:14:11 +0100

    De ultieme epische random

--- randomAlgorithm.py
+++ randomAlgorithm.py
@@ -1,7 +1,6 @@
 import sys
 import copy
 import random
-import numpy
 from collections import deque
 
 class Vehicle(object):
@@ -42,76 +41,95 @@ class Board(object):
             # put vehicles on board
             if orientation == 'H':
                 for i in range(vehicle.length):
-                    board[x+i][y] = id
+                    board[y][x+i] = id
             else:
                 for i in range(vehicle.length):
-                    board[x][y+i] = id
+                    board[y+i][x] = id
         return board
+        
+    def get_string(self, configuration):
+        stringconfiguration = ""
+        for vehicles in configuration:
+            stringvehicle = str(vehicles.id) + str(vehicles.x) + str(vehicles.y) + str(vehicles.orientation)
+            stringconfiguration += stringvehicle
+        return stringconfiguration
 
     def get_moves(self, configuration):
         board = self.get_board(configuration)
     
         # create list for new configurations
         children = []
-            
+
         for vehicle in configuration:
             if vehicle.orientation == 'H':
-                n = 1
-                a= 0
-                while a < n:
-                    # check if vehicle can move left & change x coordinate
-                    a += 1
-                    if vehicle.x - a >= 0 and board[vehicle.x - a][vehicle.y] == '_':
-                        n += 1
-                        new_configuration = copy.deepcopy(configuration)
-                        for copied_vehicle in new_configuration:
-                            if copied_vehicle.id == vehicle.id:
-                                copied_vehicle.x -=  a
-                                children.append(new_configuration)
-                    
-                n = 1
-                a = 0
-                # check if vehicle can move right & change x coordinate
-                while a < n:
-                    a += 1
-                    if vehicle.x+a + vehicle.length-1 < self.width and board[vehicle.x+a + vehicle.length-1][vehicle.y] == '_':
-                        n += 1
-                        new_configuration = copy.deepcopy(configuration)
-                        for copied_vehicle in new_configuration:
-                            if copied_vehicle.id == vehicle.id:
-                                copied_vehicle.x +=  a
-                                children.append(new_configuration)
+                a = 1
+                # check for left
+                if vehicle.x - a >= 0 and board[vehicle.y][vehicle.x - a] == '_':
+                    while vehicle.x - a >= 0 and board[vehicle.y][vehicle.x - a] == '_':
+                        maximum = a
+                        a += 1
+                    # copy configuration
+                    new_configuration = configuration[:]
+                    for copied_vehicle in new_configuration:
+                        if copied_vehicle.id == vehicle.id:
+                            index = new_configuration.index(vehicle)
+                            new_vehicle = copy.deepcopy(vehicle)
+                            new_vehicle.x -=  maximum
+                            new_configuration[index] = new_vehicle
+                            children.append(new_configuration)
+
+                # check for right
+                b = 1
+                if vehicle.x + b + vehicle.length - 1 < self.width and board[vehicle.y][vehicle.x + b + vehicle.length - 1] == '_':
+                    while vehicle.x + b + vehicle.length - 1 < self.width and board[vehicle.y][vehicle.x + b + vehicle.length - 1] == '_':
+                        maximum = b
+                        b += 1
+                    # copy configuration
+                    new_configuration = configuration[:]
+                    # find movable car
+                    for copied_vehicle in new_configuration:
+                        if copied_vehicle.id == vehicle.id:
+                            index = new_configuration.index(vehicle)
+                            new_vehicle = copy.deepcopy(vehicle)
+                            new_vehicle.x +=  maximum
+                            new_configuration[index] = new_vehicle
+                            children.append(new_configuration)
 
             # move vehicles vertically
             if vehicle.orientation == 'V':
-                n = 1
-                a = 0
-                while a < n:
-                    a += 1
-                    # check if vehicle can move up & change y coordinate
-                    if vehicle.y -a  >= 0 and board[vehicle.x][vehicle.y - a] == '_':
-                        n +=1
-                        new_configuration = copy.deepcopy(configuration)
-                        for copied_vehicle in new_configuration:
-                            if copied_vehicle.id == vehicle.id:
-                                copied_vehicle.y -= a
-                                children.append(new_configuration)
+                c = 1
+                if vehicle.y - c  >= 0 and board[vehicle.y - c][vehicle.x] == '_':
+                    while vehicle.y - c >= 0 and board[vehicle.y - c][vehicle.x] == '_':
+                        maximum = c
+                        c += 1
+                    # copy configuration
+                    new_configuration = configuration[:]
+                    for copied_vehicle in new_configuration:
+                        if copied_vehicle.id == vehicle.id:
+                            index = new_configuration.index(vehicle)
+                            new_vehicle = copy.deepcopy(vehicle)
+                            new_vehicle.y -= maximum
+                            new_configuration[index] = new_vehicle
+                            children.append(new_configuration)
 
                 # check if vehicle can move down & change y coordinate
-                n = 1
-                a = 0
-                while a < n:
-                    a += 1
-                    if vehicle.y + a + vehicle.length-1 < self.height and board[vehicle.x][vehicle.y + a + vehicle.length-1] == '_':
-                        n += 1
-                        new_configuration = copy.deepcopy(configuration)
-                        for copied_vehicle in new_configuration:
-                            if copied_vehicle.id == vehicle.id:
-                                copied_vehicle.y += a 
-                                children.append(new_configuration)
-        
-        #randomvehiclearray = random.choice(children) 
+                d = 1
+                if vehicle.y + d + vehicle.length - 1 < self.height and board[vehicle.y + d + vehicle.length - 1][vehicle.x] == '_':
+                    while vehicle.y + d + vehicle.length - 1 < self.height and board[vehicle.y + d + vehicle.length - 1][vehicle.x] == '_':
+                        maximum = d
+                        d += 1
+                    # copy configuration
+                    new_configuration = configuration[:]
+                    for copied_vehicle in new_configuration:
+                        if copied_vehicle.id == vehicle.id:
+                            index = new_configuration.index(vehicle)
+                            new_vehicle = copy.deepcopy(vehicle)
+                            new_vehicle.y += maximum
+                            new_configuration[index] = new_vehicle
+                            children.append(new_configuration)
         return children
+        
+        
 
 # https://jeremykun.com/tag/breadth-first-search/
 
@@ -121,47 +139,36 @@ def Random(configuration):
     short_memory_archive = {}
     counter = 0
     queue = deque([configuration])
-    stringStartingConfiguration = ""
 
     # create string of starting configuration for archive
-    for vehicles in configuration:
-        stringvehicle = str(vehicles.id) + str(vehicles.x) + str(vehicles.y) + str(vehicles.orientation)
-        stringStartingConfiguration += stringvehicle
+    stringStartingConfiguration = board.get_string(configuration)
            
     archive[stringStartingConfiguration] = None
 
     while len(queue) > 0:
-        if counter > 500:
+        if counter > 51:
             break
         current_configuration = queue.pop()
         counter += 1
-        stringcars = ""
-        winning_state = 0
         
-        if short_memory_archive.items > 1:
+        if short_memory_archive.items > 2:
             short_memory_archive.pop
 
         # create string of currently checked configuration
-        for vehicles in current_configuration:
-            stringvehicle = str(vehicles.id) + str(vehicles.x) + str(vehicles.y) + str(vehicles.orientation)
-            if stringvehicle == 'x105H':
-                winning_state = 1
-            stringcars += stringvehicle
-            
-        if winning_state == 1:
+        stringcars = board.get_string(current_configuration)
+        if 'x42H' in stringcars:
             steps_taken = 0
             parent = archive[stringcars]
             while archive[parent] != None:
                 i = 0
                 child = parent
                 parent = archive[parent]
-                for bla in child:
-                    if parent[i] != child[i] and str.isalpha(parent[i-1]):
-                        print "from", child[i-1]+child[i]+child[i+1]+child[i+2], "to", parent[i-1]+parent[i]+parent[i+1]+parent[i+2]
+                 # check string for different position of cars
+                for i in range(len(child)):
+                    if parent[i] != child[i] and str.isalpha(parent[i - 1]):
+                        print "from", child[i - 1:i + 3], "to", parent[i - 1:i + 3]
                     elif parent[i] != child[i]:
-                        print "from", child[i-2]+child[i-1]+child[i]+child[i+1], "to", parent[i-2]+parent[i-1]+parent[i]+parent[i+1]
-                    # update the index
-                    i += 1
+                        print "from", child[i - 2:i + 2], "to", parent[i - 2:i + 2]
                 # update steps_taken
                 steps_taken += 1
             #print "Totaal aantal gezette stappen:", steps_taken
@@ -174,16 +181,13 @@ def Random(configuration):
             if b > 5:
                 break
             children = []
-            stringCurrentConfiguration = ""
             children = board.get_moves(current_configuration)
             randomchoice = random.choice(children)
         
             #randomvehiclearray = numpy.random.choice(children)
-            for cars in randomchoice:
-                stringvehicles = str(cars.id) + str(cars.x) + str(cars.y) + str(cars.orientation)
-                stringCurrentConfiguration += stringvehicles
+            stringCurrentConfiguration = board.get_string(randomchoice)
 
-            if (stringCurrentConfiguration not in short_memory_archive):
+            if stringCurrentConfiguration not in short_memory_archive and stringCurrentConfiguration != stringStartingConfiguration:
                 queue.appendleft(randomchoice)
                 archive[stringCurrentConfiguration] = stringcars
                 short_memory_archive[stringCurrentConfiguration] = None
@@ -209,12 +213,14 @@ if __name__ == '__main__':
             configuration.append(vehicle)
 
         # create board
-        board = Board(12,12, configuration)
+        board = Board(6,6, configuration)
         print board
         
         results = []
         # run algorithme
         for i in range(0, 1000):
             print "iteration: ", i
-            results.append(Random(configuration))
-            print sorted(results)      
+            result = (Random(configuration))
+            if result != None:
+                results.append(result)
+                print sorted(results)      
